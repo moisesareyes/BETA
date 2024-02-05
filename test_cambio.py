9ed0930c6e945f9448fddcdb9e25eda4f5d3ed89
Moises Reyes <moisesareyes@gmail.com>
2024-02-05 19:32:30 -0400

    cambio por funciones separadas

--- test_cambio.py
+++ test_cambio.py
@@ -10,43 +10,30 @@ mydb = mysql.connector.connect(
 
 bcv=float(36.23)
 usuario="ZRO-42111"
-selec="CRYPTO"
+tipo="CRYPTO"
 crp=47
 crs=mydb.cursor()
-def main():
-    sql=f"SELECT `tipo`, `cantidad` FROM `billetera` WHERE `poseedor`='{usuario}'"
+def cambio_cry(usuario, tipo,tasa, dispo):
+    sql=f"SELECT * FROM `billetera` WHERE `poseedor`='{usuario}' AND  `tipo`='{tipo}' "
     crs.execute(sql)
-    all=crs.fetchall()
-    print("Seleccione tipo de billetera al cambio: ")
-    f=0
-    ops=set()
-    for bill in all :
-        f+=1
-        if not selec==bill[0]:
-            print(f"- {f}. {bill[0]} (Saldo disponible: {bill[1]})")
-            ops.add((bill[0],float(bill[1]),f))
-        else: continue
-    print(ops)
-    new=input("-> ")
-    for op in ops:
-        if int(new)==op[2] and op[0]=="Bs.D":
-            print("a")
-        elif int(new)==op[2] and op[0]=="USD":
-            print("b")
-
-def cambio_personalizado(pers,ops):
-    for op in ops:
-        maxi=op[1]/7
-        enter=input(f"""
-Tiene un total de {op[1]} disponible, cuanto desea cambiar?
-->
-""")
-        if enter<=pers:
-            llave=enter/pers
-            key=llave-op[1]
-            sql=f"UPDATE `billetera` SET `cantidad`='{llave}' WHERE `poseedor`= '{usuario}' AND `tipo`='{selec}'"
-            sqlP=f"UPDATE `billetera` SET `cantidad`='{key}' WHERE `poseedor`= '{usuario}' AND `tipo`='{op[0]}'"
-            crs.execute(sql)
-            crs.execute(sqlP)
-            mydb.commit()
+    inf=crs.fetchone()
+    sql=f"SELECT * FROM `billetera` WHERE `poseedor`='{usuario}' AND  `tipo`='USD' "
+    crs.execute(sql)
+    det=crs.fetchone()
+    print(f"{inf} vs {det}")
+    if det[2]=="Bs.D" and tipo=="CRYPTO": 
+         print("NO SE PUEDEN COMPRAR CRYPTOS CON BOLIVARES")
+         sys.exit(0)
+    cant_max=float(det[3])
+    print(tasa)
+    print(f"{cant_max} + {5}")
+    cambio=float(input(f"Ingrese cuanto {det[2]} desea cambiar: "))
+    if cambio>5 and cambio<cant_max:
+        new= cambio/tasa
+        print(new)
+#cambio_cry(usuario,tipo,42500.38,1)
+#################################### Bolivar a $$$$
+def cambio_dlr(usuario,tasa,dispo):
+    print(f"{usuario} + {tasa} + {dispo}")
+cambio_dlr(usuario,bcv,10000)
 mydb.close()
